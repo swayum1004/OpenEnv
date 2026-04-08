86b1beffcd18d222cc57a29d5c4a8813b730678c
swayum1004 <161558039+swayum1004@users.noreply.github.com>
2026-04-08 23:23:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from server import app  # import your FastAPI app
+
+def main():
+    import uvicorn
+    uvicorn.run("server:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
